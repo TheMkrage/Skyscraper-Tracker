b969a808fe94e04b58c7b29f9b3b05439454d3f6
Matthew Krager <matthewkrager@gmail.com>
2020-11-26 21:32:10 -0800

    initial

---
+++ lib/skyscraper_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SkyscraperListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SkyscraperListPage();
+  }
+}
+
+class _SkyscraperListPage extends State<SkyscraperListPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
