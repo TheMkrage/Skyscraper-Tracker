deae3b02c54baad9c7009c2bfd5e5986d329b45f
Matthew Krager <matthewkrager@gmail.com>
2020-11-29 21:54:43 -0800

    can read text field

--- lib/new_skyscraper_page.dart
+++ lib/new_skyscraper_page.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
 class NewSkyscraperPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("New Skyscraper"),
       ),
-      body: Text('yes'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: 'Skyscraper Name'),
+              ),
+              ElevatedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  print(_formKey);
+                  print(_formKey.currentState);
+                  print(_controller.text);
+                  print(_controller.value);
+                },
+              )
+            ],
+          ),
+          key: _formKey,
+        ),
+      ),
     );
   }
 }
